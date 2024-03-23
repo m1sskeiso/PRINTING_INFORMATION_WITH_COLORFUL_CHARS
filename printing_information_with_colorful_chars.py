72c43594e6268b2fd773947b1fda34016be87a1e
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-03-23 17:09:01 +0700

    main program

--- printing_information_with_colorful_chars.py
+++ printing_information_with_colorful_chars.py
@@ -1,5 +1,12 @@
 # Main program
     # Prompt user to input their name
+name = input("Enter your name: ")
+
     # Prompt user to input their dream job
+dream_job = input("Enter your dream job: ")
+
     # Print name 
+print("\nYour name:", name)
+
     # Print dream job 
+print("\nYour dream job: ", dream_job)
