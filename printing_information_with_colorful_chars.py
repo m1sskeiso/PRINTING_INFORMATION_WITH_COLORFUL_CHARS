5df897ee49c6a38802ee2cb56bca0015b9985c8a
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-03-23 17:14:43 +0700

    import colorama and inputing colors to the chars

--- printing_information_with_colorful_chars.py
+++ printing_information_with_colorful_chars.py
@@ -1,3 +1,13 @@
+import colorama
+
+# Initialize colorama
+colorama.init()
+
+# Function to print text in rainbow colors with bold formatting
+def print_rainbow_bold(text):
+    colors = [colorama.Fore.RED, colorama.Fore.YELLOW, colorama.Fore.GREEN, colorama.Fore.CYAN, colorama.Fore.BLUE, colorama.Fore.MAGENTA]
+    rainbow_text = ''
+    
 # Main program
     # Prompt user to input their name
 name = input("Enter your name: ")
